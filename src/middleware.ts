21b6c4660b0ef076532a442e25f012cb69e1a646
sovon1 <sovon2018@gmail.com>
2026-03-10 17:39:22 +0600

    alright

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * - public folder
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|sw\\.js|manifest\\.json|offline\\.html|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 };
